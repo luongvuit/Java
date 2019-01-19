e31fd4372a6966e3d84277f6da7e64efdfbff66a
LUONG VU <luongvu.system@gmail.com>
2019-01-19 22:27:56 +0700

    Java Template MVC V 4.0

---
+++ Java MVC Core/src/lib/MD5.java
@@ -0,0 +1,55 @@
+/*
+ * ---Welcome to Coder's House--- * 
+
+ * ------------------------------ * 
+
+ * LUONG VU  * 
+ * DUY NGUYEN * 
+ * HOANG ANH * 
+ * PHUONG MY  * 
+
+ * ------------------------------ * 
+
+ * Add: 57 - 53 - Tan Phong W - D7 - HCMC  * 
+ * Phone Nu: +84 0935710974 * 
+ * Skype: luongvu.work@gmail.com * 
+ * Email: luongvu.work@gmail.com * 
+
+ * ------------------------------ * 
+
+ * Copyright (C) 2019 LUONG VU TECHNOLOGY
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package lib;
+
+/**
+ *
+ * @author LUONG VU TECHNOLOGY
+ */
+public class MD5 {
+    public String md5(String md5) {
+        try {
+            java.security.MessageDigest md = java.security.MessageDigest.getInstance("MD5");
+            byte[] array = md.digest(md5.getBytes());
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < array.length; ++i) {
+                sb.append(Integer.toHexString((array[i] & 0xFF) | 0x100).substring(1, 3));
+            }
+            return sb.toString();
+        } catch (java.security.NoSuchAlgorithmException e) {
+        }
+        return null;
+    }
+}
