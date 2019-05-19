36c48538d10d496ece35053b121edc8954f3bae5
LUONG VU <luongvu.work@gmail.com>
2019-05-19 18:45:52 +0700

    Update
    Update

---
+++ 04 Java EE/EJBModule1/src/java/richard/LibrarySessionBean.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package richard;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.ejb.Stateless;
+
+/**
+ *
+ * @author luong
+ */
+@Stateless
+public class LibrarySessionBean implements LibrarySessionBeanRemote {
+   
+   List<String> bookShelf;    
+   
+   public LibrarySessionBean() {
+      bookShelf = new ArrayList<String>();
+   }
+    
+   public void addBook(String bookName) {
+      bookShelf.add(bookName);
+   }    
+ 
+   public List<String> getBooks() {
+      return bookShelf;
+   }
+}
